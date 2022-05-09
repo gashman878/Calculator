02c44fd61f989f5760dbc16341ab605d0d3be223
Gashman <gashman878@gmail.com>
2022-05-10 00:26:07 +0100

    Changes

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ function myCalculator($num01, $oper, $num02){
             $sum = $num01 + $num02;
             break;
         case "sub":
-            $sum = $num01 + $num02;
+            $sum = $num01 - $num02;
             break;
         default:
             $sum = "There was an error!";
