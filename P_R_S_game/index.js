feed7f9d5f980bb24607ae4fc4ed980bc80bce5b
gashman878 <gashman878@gmail.com>
2025-05-15 12:57:47 +0100

    thanks

--- P_R_S_game/index.js
+++ P_R_S_game/index.js
@@ -1,12 +1,21 @@
+// const display = document.getElementById('screen_detail');
 
 function solve(pickedNumber) {
-    let screenValue = document.querySelector('.screen_detail').innerHTML;
-    console.log(screenValue);
-    console.log(pickedNumber);
-    if (screenValue === 0) {
-        console.log(screenValue);
-        console.log(pickedNumber);
-    } else {
-        
+    const display = document.getElementById('screen_detail');
+    display.value += pickedNumber;
+}
+function clearPlease() {
+    document.getElementById('screen_detail').value = "";
+
+}
+function calculate() {
+    try {
+        let evaluate = document.getElementById('screen_detail').value;
+        evaluate = eval(evaluate);
+        document.getElementById('screen_detail').value = evaluate;
+    } catch (error) {
+        document.getElementById('screen_detail').value = "Error";
     }
+    
+    
 }
