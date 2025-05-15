d6fb847100b465256f7c62ea166265021d6bb889
gashman878 <gashman878@gmail.com>
2025-05-15 11:21:29 +0100

    again

---
+++ P_R_S_game/index.js
@@ -0,0 +1,12 @@
+
+function solve(pickedNumber) {
+    let screenValue = document.querySelector('.screen_detail').innerHTML;
+    console.log(screenValue);
+    console.log(pickedNumber);
+    if (screenValue === 0) {
+        console.log(screenValue);
+        console.log(pickedNumber);
+    } else {
+        
+    }
+}
